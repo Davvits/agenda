1d508bdb56bae2f2b7c2e3b1fd247cff5cbee294
davi <kaliflamel@gmail.com>
2023-03-24 19:03:22 -0300

    update dos contatos mais delete

--- src/controllers/contatoController.js
+++ src/controllers/contatoController.js
@@ -20,6 +20,7 @@ exports.register = async (req,res) => {
 
         req.flash('success', "Contato cadastrado com sucesso")
         req.session.save(() => res.redirect(`/contato/index/${contato.contato._id}`));
+        return;
 
     } catch (error) {
         console.log(error)
@@ -36,4 +37,45 @@ exports.editIndex = async function(req,res) {
 
 
     res.render('contato', { contato })
+}
+
+exports.edit = async function(req,res) {
+
+        try{
+            if(!req.params.id) return render('404');
+            const contato = new Contato(req.body);
+            await contato.edit(req.params.id)
+        
+            if(contato.errors.length > 0){
+                req.flash('errors', contato.errors)
+                req.session.save(() => res.redirect('/contato/index'));
+                return;
+            }
+        
+            req.flash('success', "Contato atualizado com sucesso")
+            req.session.save(() => res.redirect(`/contato/index/${contato.contato._id}`));
+            return;
+        }catch(e) {
+            console.log(e)
+            res.render(`404`)
+        }
+    }
+
+exports.delete = async function(req,res) {
+    try{
+        if(!req.params.id) return res.render('404')
+     
+        const contato = await Contato.delete(req.params.id)
+    
+        if(!contato) return res.render('404')
+    
+    
+        req.flash('success', 'Contato apagado com sucesso.');
+        req.session.save(() => res.redirect('back'));
+        return;
+
+    }catch(e) {
+        console.log(e)
+        res.render('404')
+    }
 }
