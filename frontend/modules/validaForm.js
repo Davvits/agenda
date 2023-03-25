c7eb0bb15c45270404b77e902293a497e1f791d7
davi <kaliflamel@gmail.com>
2023-03-25 17:41:02 -0300

    add dos modulos js

---
+++ frontend/modules/validaForm.js
@@ -0,0 +1,39 @@
+import validator from "validator"
+
+export default class Sign {
+    constructor(formClass){
+        this.form = document.querySelector(formClass)
+    }
+
+    init(){
+        this.events()
+    }
+
+    events(){
+        if(!this.form) return
+        this.form.addEventListener('submit', e => {
+        e.preventDefault();
+        this.validate(e)
+        })
+    }
+
+    validate(e){
+        const el = e.target;
+        const emailInput = el.querySelector('input[name="email"]');
+        const senhaInput = el.querySelector('input[name="password"]');
+        let error = false;
+        if(!validator.isEmail(emailInput.value)){
+            alert(`Email invalido`)
+            error = true
+        }
+        if(senhaInput.value.length < 3 || senhaInput.value.length > 50){
+            alert('Senha precisa ter entre 3 a 50 caracteres')
+            error = true
+        }
+        if(!error) el.submit()
+        
+        console.log(emailInput.value,senhaInput.value)
+    }
+}
+
+//commit
